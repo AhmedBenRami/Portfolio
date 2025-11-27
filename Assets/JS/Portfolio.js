8f6cac5d78d314d84a8ba269924ae1c645525585
Ahmed <benrami.ahmed2005@gmail.com>
2025-11-27 17:56:51 +0100

    corrected the uloaded images error

--- Assets/JS/Portfolio.js
+++ Assets/JS/Portfolio.js
@@ -67,7 +67,7 @@
 
 // // }
 
-// load_images("/Assets/JS/JSON/Portfolio_gallery.json", "main section.gallery .photos").then(() => {
+// load_images("Assets/JS/JSON/Portfolio_gallery.json", "main section.gallery .photos").then(() => {
 
 //     let wrappers = document.querySelectorAll("main section.gallery .photos .photo_item");
 
@@ -127,67 +127,67 @@ class ImageGallery {
 
 let image_array = [
     {
-        "url": "/Assets/Images/Portfolio/_ (1).jpeg",
+        "url": "Assets/Images/Portfolio/_ (1).jpeg",
         "alt": "Portfolio image: _ (1).jpeg"
     },
     {
-        "url": "/Assets/Images/Portfolio/exerc1_card3.jpg",
+        "url": "Assets/Images/Portfolio/exerc1_card3.jpg",
         "alt": "Portfolio image: exerc1_card3.jpg"
     },
     {
-        "url": "/Assets/Images/Portfolio/pexels-filiamariss-9417212.jpg",
+        "url": "Assets/Images/Portfolio/pexels-filiamariss-9417212.jpg",
         "alt": "Portfolio image: pexels-filiamariss-9417212.jpg"
     },
     {
-        "url": "/Assets/Images/Portfolio/exerc1_card1.jpg",
+        "url": "Assets/Images/Portfolio/exerc1_card1.jpg",
         "alt": "Portfolio image: exerc1_card1.jpg"
     },
     {
-        "url": "/Assets/Images/Portfolio/exerc1_card4.jpg",
+        "url": "Assets/Images/Portfolio/exerc1_card4.jpg",
         "alt": "Portfolio image: exerc1_card4.jpg"
     },
     {
-        "url": "/Assets/Images/Portfolio/pexels-nemanja-vucic-2157177226-34592502.jpg",
+        "url": "Assets/Images/Portfolio/pexels-nemanja-vucic-2157177226-34592502.jpg",
         "alt": "Portfolio image: pexels-nemanja-vucic-2157177226-34592502.jpg"
     },
     {
-        "url": "/Assets/Images/Portfolio/land-o-lakes-inc-BvZYHz9TeCk-unsplash.jpg",
+        "url": "Assets/Images/Portfolio/land-o-lakes-inc-BvZYHz9TeCk-unsplash.jpg",
         "alt": "Portfolio image: land-o-lakes-inc-BvZYHz9TeCk-unsplash.jpg"
     },
     {
-        "url": "/Assets/Images/Portfolio/kimono.jpeg",
+        "url": "Assets/Images/Portfolio/kimono.jpeg",
         "alt": "Portfolio image: kimono.jpeg"
     },
     {
-        "url": "/Assets/Images/Portfolio/pexels-oleksandr-plakhota-1270583835-34607826.jpg",
+        "url": "Assets/Images/Portfolio/pexels-oleksandr-plakhota-1270583835-34607826.jpg",
         "alt": "Portfolio image: pexels-oleksandr-plakhota-1270583835-34607826.jpg"
     },
     {
-        "url": "/Assets/Images/Portfolio/pexels-miken-34533233.jpg",
+        "url": "Assets/Images/Portfolio/pexels-miken-34533233.jpg",
         "alt": "Portfolio image: pexels-miken-34533233.jpg"
     },
     {
-        "url": "/Assets/Images/Portfolio/_.jpeg",
+        "url": "Assets/Images/Portfolio/_.jpeg",
         "alt": "Portfolio image: _.jpeg"
     },
     {
-        "url": "/Assets/Images/Portfolio/pexels-mxmaven-2156962353-34496646.jpg",
+        "url": "Assets/Images/Portfolio/pexels-mxmaven-2156962353-34496646.jpg",
         "alt": "Portfolio image: pexels-mxmaven-2156962353-34496646.jpg"
     },
     {
-        "url": "/Assets/Images/Portfolio/face.jpeg",
+        "url": "Assets/Images/Portfolio/face.jpeg",
         "alt": "Portfolio image: face.jpeg"
     },
     {
-        "url": "/Assets/Images/Portfolio/Mads Mikkelsen was born with these glasses on btw.jpeg",
+        "url": "Assets/Images/Portfolio/Mads Mikkelsen was born with these glasses on btw.jpeg",
         "alt": "Portfolio image: Mads Mikkelsen was born with these glasses on btw.jpeg"
     },
     {
-        "url": "/Assets/Images/Portfolio/porche.jpeg",
+        "url": "Assets/Images/Portfolio/porche.jpeg",
         "alt": "Portfolio image: porche.jpeg"
     },
     {
-        "url": "/Assets/Images/Portfolio/exerc1_card2.jpg",
+        "url": "Assets/Images/Portfolio/exerc1_card2.jpg",
         "alt": "Portfolio image: exerc1_card2.jpg"
     }
 ];
