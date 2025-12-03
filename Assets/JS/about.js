a598dbc44728e2e3ae00c2ebbc5dd188bbd7b6ee
amin <mohamedaminroughi40@gmail.com>
2025-12-03 15:59:44 +0100

    ...

---
+++ Assets/JS/about.js
@@ -0,0 +1,28 @@
+let span1 = document.getElementById("sts1")
+        let span2 = document.getElementById("sts2")
+        let span3 = document.getElementById("sts3")
+
+
+        let container = document.querySelector(".achievements2")
+        function shffle(max, span) {
+            span.textContent = 0
+            let end = setInterval(function () {
+                span.textContent = parseInt(span.textContent) + 1
+                if (parseInt(span.textContent) >= max) {
+                    clearInterval(end)
+                }
+
+            }, 30)
+
+        }
+
+        const observer = new IntersectionObserver((entries) => {
+            if (entries[0].isIntersecting) {
+                shffle(100, span1)
+                shffle(21, span2)
+                shffle(37, span3)
+                observer.disconnect();  // run only once
+            }
+        });
+        observer.observe(container);
+
